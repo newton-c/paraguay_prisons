dbf50d5b3edbfd0fa226a8e3eab80e2938926b7f
newton-c <newton.christop@gmail.com>
2023-06-28 17:30:20 -0300

    importing data, x-axis built

--- scripts/overpop_bar.js
+++ scripts/overpop_bar.js
@@ -1,5 +1,5 @@
 
-*/ this bar chart shows the population of prisons as a percentage of their 
+/* this bar chart shows the population of prisons as a percentage of their 
 official capacity, to visualize how over (or occasionally) populated they
 are */
 
@@ -8,9 +8,22 @@ const margin = {top: 20, right: 20, bottom: 20, left: 20},
 	width = 640 - margin.left - margin.right,
 	height = 450 - margin.top - margin.bottom
 
-const svg.select("#overpop_bar")
+const svg = d3.select("#overpop_bar")
 	.append("svg")
 		.attr("height", height + margin.top + margin.bottom)
 		.attr("width", width + margin.left + margin.right)
 	.append("g")
 		.attr("transform", `translate(${margin.left}, ${margin.top})`)
+
+d3.csv("https://raw.githubusercontent.com/newton-c/paraguay_prisons/main/data/overpop_data.csv").then(function(data) {
+	data.overpopulation = Number(data.overpopulation)
+		console.log(data);
+
+	const x = d3.scaleLinear()
+		.domain([0, d3.max(data, function(d) { return +d.overpopulation })])
+		.range([0, width])
+
+	svg.append("g")
+		.attr("transform", `translate(0, ${height})`)
+		.call(d3.axisBottom(x))
+})
